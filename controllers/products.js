b42ed8ee05d87f071ca3c37abf8b6dff6340186f
Sander van Rijsoort <hallo@svanrijsoort.nl>
2021-04-04 19:39:38 +0100

    removed console log

--- controllers/products.js
+++ controllers/products.js
@@ -4,7 +4,6 @@ module.exports = {
   getAllProducts: async (req, res, next) => {
     try {
       const products = await Product.find();
-      console.log("testing");
       res.status(200).json(products);
     } catch (error) {
       res.status(400).send(error);
