c8650b2cef6d269bd4497c3ccb6f85a4849e667b
Sander van Rijsoort <s.rijsoort@gmail.com>
2021-04-12 14:27:08 +0100

    created a login endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,7 +10,7 @@ module.exports = {
     const userExists = await User.findOne({ email: email });
 
     if (userExists) {
-      res.status(400).json("This user already exists");
+      return res.status(400).json("This user already exists");
     }
 
     try {
@@ -20,17 +20,27 @@ module.exports = {
       const newUser = new User({ email, password: hashedPassword });
       const savedUser = await newUser.save();
 
-      res.status(200).json(savedUser);
+      return res.status(200).json(savedUser);
     } catch (error) {
-      res.status(400).send(error);
+      return res.status(400).send(error);
     }
   },
 
   login: async (req, res, next) => {
-    try {
-      res.status(200).json("login endpoint");
-    } catch (error) {
-      res.status(400).send(error);
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).send("The user was not found.");
     }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return res.status(400).send("You entered a wrong password");
+    }
+
+    res.status(200).send("You logged in succesfully.");
   }
 };
